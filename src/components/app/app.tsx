32ad1b18b1dee584a9b1757e075e50b5ea5e0aaa
Julia Antonova <1306577@mail.ru>
2021-12-17 10:14:20 +0100

    Добавлена верстка главной страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { AppRoute } from '../../const';
+import MainScreen from '../main-screen/main-screen';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
