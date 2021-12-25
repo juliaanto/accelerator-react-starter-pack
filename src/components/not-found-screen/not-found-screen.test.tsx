6154fe86a66b13214438ca475eab53c353ce950e
Julia Antonova <1306577@mail.ru>
2021-12-25 20:09:32 +0100

    Добавлен тест для компонента NotFoundScreen

---
+++ src/components/not-found-screen/not-found-screen.test.tsx
@@ -0,0 +1,39 @@
+import {Route, Router, Switch} from 'react-router-dom';
+import {render, screen} from '@testing-library/react';
+
+import NotFoundScreen from './not-found-screen';
+import {createMemoryHistory} from 'history';
+import userEvent from '@testing-library/user-event';
+
+const history = createMemoryHistory();
+
+describe('Component: NotFoundScreen', () => {
+  it('should render correctly', () => {
+    render(
+      <Router history={history}>
+        <NotFoundScreen />
+      </Router>);
+
+    expect(screen.getByText(/404 Not Found/i)).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+
+  it('should redirect to root url when user clicked to link', () => {
+    history.push('/fake');
+    render(
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact>
+            <h1>This is main page</h1>
+          </Route>
+          <Route>
+            <NotFoundScreen />
+          </Route>
+        </Switch>
+      </Router>);
+
+    expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('link'));
+    expect(screen.getByText(/This is main page/i)).toBeInTheDocument();
+  });
+});
