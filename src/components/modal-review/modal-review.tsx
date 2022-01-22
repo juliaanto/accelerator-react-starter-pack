07369efb6cd2d740ddfb602c2dc3fb7026eba026
Julia Antonova <1306577@mail.ru>
2022-01-22 21:32:36 +0300

    Отменен последний коммит

--- src/components/modal-review/modal-review.tsx
+++ src/components/modal-review/modal-review.tsx
@@ -97,7 +97,6 @@ function ModalReview(props: ModalReviewProps): JSX.Element {
     setRate(Number(checkedRadio.value));
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleRateKeyDown = (evt: any) => {
 
     document.querySelectorAll('input[name=rate]:checked').forEach((element) => element.removeAttribute('checked'));
